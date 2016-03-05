cbd1672c03fc0f85cd8bc4adf85eddf5eeee8f02
bk2dcradle <ankitsultana@gmail.com>
2016-03-06 02:20:51 +0530

    logic done

--- includes/js/lib/app.js
+++ includes/js/lib/app.js
@@ -23,53 +23,6 @@ var redditapp = {
         temp = '{"subreddit": "' + subreddit + '", "url": "' + url + '"}'
         return JSON.parse(temp)
     },
-    addCategory: function(input) {
-        var resp = this.checkCategory(input)
-        if (resp == 1) {
-            // error
-            this.failure()
-        } else {
-            localStorage.setItem(resp['subreddit'], resp['url'])
-            console.log(resp)
-        }
-        this.printAllCategory()
-    },
-    printAllCategory: function() {
-        var iter
-        for (iter = 0; iter < localStorage.length; iter++) {
-            console.log(localStorage.getItem(localStorage.key(iter)))
-        }
-    },
-    deleteCategory: function(input) {
-
-    },
-    checkCategory: function(input) {
-        var xhr = new XMLHttpRequest(), url = 'https://www.reddit.com/' + input, status, jsonData
-        if (url.slice(-1) == '/') {
-            url = url.slice(0, -1)
-        }
-        xhr.open('GET', url + '/top/.json?limit=1', false)
-        xhr.send()
-        status = xhr.status
-        console.log(status)
-        if (status != 200) {
-            return 1
-        } else {
-            console.log(xhr.responseText)
-            jsonData = JSON.parse(xhr.responseText)
-            console.log(jsonData["data"]["children"][0]["data"]["subreddit"])
-            return {
-                "subreddit": jsonData["data"]["children"][0]["data"]["subreddit"],
-                "url": url
-            }
-        }
-    },
-    failure: function() {
-        console.log('Failed to add, please try again')
-    },
-    clear: function() {
-        localStorage.clear()
-    },
     randomInt: function(min, max) {
         return Math.floor(Math.random() * (max - min) + min);
     }
---
+++ includes/js/lib/options.js
@@ -0,0 +1,79 @@
+$(document).ready(function() {
+    options.displayCategories()
+})
+
+var options = {
+    displayError: function() {
+        $('#error-container').show()
+    },
+    dismissError: function() {
+        $('#error-container').hide()
+    },
+    displayCategories: function() {
+        $('#category-list').empty()
+        var len = localStorage.length, iter, subreddit
+        for (iter = 0; iter < len; iter++) {
+            subreddit = localStorage.key(iter)
+            $('#category-list').append('<li><a class="delete-category" id="lement' + iter + '" href="#"><span class="icon-cross"></span></a>\
+                                        <p id="element' + iter + '">' + subreddit + '</p></li>')
+        }
+    },
+    addCategory: function(input) {
+        var resp = this.checkCategory(input)
+        if (resp == 1) {
+            this.displayError()
+        } else {
+            localStorage.setItem(resp['subreddit'], resp['url'])
+            console.log('Added: ' + resp)
+            this.displayCategories()
+        }
+    },
+    checkCategory: function(input) {
+        var xhr = new XMLHttpRequest(), url = 'https://www.reddit.com/' + input, status, jsonData
+        if (url.slice(-1) == '/') {
+            url = url.slice(0, -1)
+        }
+        xhr.open('GET', url + '/top/.json?limit=1', false)
+        xhr.send()
+        status = xhr.status
+        console.log(status)
+        if (status != 200) {
+            return 1
+        } else {
+            console.log(xhr.responseText)
+            jsonData = JSON.parse(xhr.responseText)
+            console.log(jsonData["data"]["children"][0]["data"]["subreddit"])
+            return {
+                "subreddit": jsonData["data"]["children"][0]["data"]["subreddit"],
+                "url": url
+            }
+        }
+    },
+    deleteCategory: function(subreddit) {
+        localStorage.removeItem(subreddit)
+        this.displayCategories()
+    }
+}
+
+/*
+ * Add Listeners
+ */
+
+$('#error-container').hide()
+
+$('.dismiss-error').on('click', options.dismissError)
+$('#target').submit(function() {
+    var input = $('#target :input')
+    var values = {}
+    input.each(function() {
+        values["url"] = $(this).val()
+    })
+    options.addCategory(values["url"])
+})
+
+$(document).on('click', '.delete-category', function() {
+    var id = $(this).attr('id'), key
+    id = 'e' + id
+    key = document.getElementById(id).innerHTML
+    options.deleteCategory(key)
+})

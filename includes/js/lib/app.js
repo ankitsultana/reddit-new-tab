ce195d20fb022541fa35f93b2d7f95e02e7e25a9
bk2dcradle <ankitsultana@gmail.com>
2016-03-06 00:14:45 +0530

    Update

--- includes/js/lib/app.js
+++ includes/js/lib/app.js
@@ -2,16 +2,26 @@
  * app engine
  */
 var redditapp = {
-    fetch: function() {
-        var arr
-        arr = [1, 100, 230]
-        return this.permute(arr)
+    DOWN_LIMIT: 3,
+    fetch: function() { // return arr of JSON data
+        var jsonData, key = this.getkey(), url, subreddit, xhr, result
+        url = key["url"] + '/top/.json?limit=' + this.DOWN_LIMIT
+        subreddit = key["subreddit"]
+        xhr = new XMLHttpRequest()
+        xhr.open('GET', url, false)
+        xhr.send()
+        jsonData = JSON.parse(xhr.response)
+        result = jsonData["data"]["children"]
+        return result
     },
-    geturl: function() {
-        var i
-        for (i = 0; i < localStorage.length; i++) {
-            console.log(localStorage.getItem(localStorage.key(i)))
-        }
+    getkey: function() {
+        var i, idx, url, subreddit, temp
+        idx = this.randomInt(0, localStorage.length - 1)
+        url = localStorage.getItem(localStorage.key(idx))
+        subreddit = localStorage.key(idx)
+        console.log(subreddit), console.log(url)
+        temp = '{"subreddit": "' + subreddit + '", "url": "' + url + '"}'
+        return JSON.parse(temp)
     },
     addCategory: function(input) {
         var resp = this.checkCategory(input)
@@ -19,7 +29,15 @@ var redditapp = {
             // error
             this.failure()
         } else {
+            localStorage.setItem(resp['subreddit'], resp['url'])
             console.log(resp)
+        }
+        this.printAllCategory()
+    },
+    printAllCategory: function() {
+        var iter
+        for (iter = 0; iter < localStorage.length; iter++) {
+            console.log(localStorage.getItem(localStorage.key(iter)))
         }
     },
     deleteCategory: function(input) {
@@ -49,10 +67,10 @@ var redditapp = {
     failure: function() {
         console.log('Failed to add, please try again')
     },
-    permute: function(input) {
-
-    },
     clear: function() {
         localStorage.clear()
+    },
+    randomInt: function(min, max) {
+        return Math.floor(Math.random() * (max - min) + min);
     }
 }
--- includes/js/lib/carousel.js
+++ includes/js/lib/carousel.js
@@ -1,18 +1,73 @@
-function Carousel(items) {
-    for (var i = 0; i < items.length; i++) {
-        console.log(items[i])
+var carousel = {
+    data: [],
+    currIdx: 0,
+    len: 0,
+    setData: function(data) {
+        document.getElementById('content').innerHTML = data
+    },
+    setSubreddit: function(subreddit) {
+        document.getElementById('subreddit-name').innerHTML = subreddit
+    },
+    rotate: function(arr) {
+        var len = arr.length, temp = [], iter, hinge
+        hinge = redditapp.randomInt(0, len-1)
+        for (iter = hinge; iter < len; iter++) {
+            temp.push(arr[iter])
+        }
+        for (iter = 0; iter < hinge; iter++) {
+            temp.push(arr[iter])
+        }
+        return temp
+    },
+    go: function() {
+        this.data = redditapp.fetch()
+        this.data = this.rotate(this.data)
+        this.setData(this.getTitle(this.data[0]))
+        this.setSubreddit(this.getSubreddit(this.data[0]))
+        this.currIdx = 0
+        this.len = this.data.length
+    },
+    getNext: function() {
+        this.currIdx++
+        var currIdx = this.currIdx
+        this.setData(this.getTitle(this.data[currIdx]))
+        this.setSubreddit(this.getSubreddit(this.data[currIdx]))
+        return currIdx == this.data.length - 1
+    },
+    getPrevious: function() {
+        this.currIdx--
+        var currIdx = this.currIdx
+        this.setData(this.getTitle(this.data[currIdx]))
+        this.setSubreddit(this.getSubreddit(this.data[currIdx]))
+        return currIdx == 0
+    },
+    getTitle: function(obj) {
+        return obj["data"]["title"]
+    },
+    getSubreddit: function(obj) {
+        return obj["data"]["subreddit"]
     }
 }
 
 $(document).ready(function() {
     console.log("Enter the Carousel")
-
-    // console.log(redditapp.addCategory('r/todayilearned/'))
+    $("#prev-post").hide()
+    carousel.go()
 
     $("#next-post").on("click", function() {
-        $("#content").text("Reuters reporter taken into custody by Hamas officials in Gaza Reuters reporter taken into custody by Hamas officials in Gaza Reuters reporter taken into custody by Hamas officials in GazaReuters reporter taken into custody by Hamas officials in Gaza")
+        if (carousel.getNext()) {  // Hide next button
+            $("#next-post").hide()
+        }
+        if (carousel.len > 1 && carousel.currIdx == 1) {
+            $("#prev-post").show()
+        }
     })
     $("#prev-post").on("click", function() {
-        $("#content").text("mas officials in Gaza Reuters reporter taken into custody by Hamas officials in Gaza Reuters reporter taken into custody by Hamas officials in Gaza Reuters reporter taken into custody by Hamas officials in Gaza")
+        if (carousel.getPrevious()) {  // Hide next button
+            $("#prev-post").hide()
+        }
+        if (carousel.currIdx == carousel.len - 2) {
+            $("#next-post").show()
+        }
     })
 })
